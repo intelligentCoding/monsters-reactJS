eac97417d2f76e02b8881d6dea65a9831f970c0e
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2020-03-21 10:35:03 -0700

    Updates

---
+++ precache-manifest.670fefe5eeca59acbe3a74487be479e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4390e01a48a99a6b3fe3e638e2571cdf",
+    "url": "/monsters-reactJS/index.html"
+  },
+  {
+    "revision": "44645f0406c1fe8b000b",
+    "url": "/monsters-reactJS/static/css/main.dd6b67c2.chunk.css"
+  },
+  {
+    "revision": "2db5ba9bcf1a2f156455",
+    "url": "/monsters-reactJS/static/js/2.8663a8b1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-reactJS/static/js/2.8663a8b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44645f0406c1fe8b000b",
+    "url": "/monsters-reactJS/static/js/main.15351d85.chunk.js"
+  },
+  {
+    "revision": "0aa42f3baa1713e64166",
+    "url": "/monsters-reactJS/static/js/runtime-main.1fb55e2f.js"
+  }
+]);
